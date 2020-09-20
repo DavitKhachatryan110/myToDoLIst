67b0f2507c0554ed645b8ea70fbc57250468ff59
DavitKhachatryan110 <davitkhachatryan7@mail.ru>
2020-09-20 23:24:12 +0400

    less9

--- src/ToDoComponents/Lists.js
+++ src/ToDoComponents/Lists.js
@@ -1,13 +1,14 @@
 import React , {Component , PureComponent} from 'react'
 import {ListGroupItem , ListGroup} from 'reactstrap'
+import  '../Styles/all.css'
 
 export default class List extends PureComponent {
 
     render() {
 
-        const List = this.props.tasks.map((item) => (
+        const List = this.props.tasks.reverse().map((item) => (
            
-        <ListGroupItem  tag="button"  action key={item.todoId} onClick={this.props.handleDelete(item.todoId)}>{item}  </ListGroupItem>
+        <ListGroupItem className='listStyle' tag="button"  action key={item.todoId} onClick={() => {this.props.handleDelete(item.todoId)}}>{item.text}</ListGroupItem>
      
           ))
 
--- src/ToDoComponents/ToDo.js
+++ src/ToDoComponents/ToDo.js
@@ -15,26 +15,20 @@ class ToDo extends Component {
         };
     }
     todoId = 1
-    // handleChange = (event) => {
     
-    //     this.setState({change:event.target.value})
-        
-    // }
     handleSubmit = (text , ev) => { 
-      //ev.preventDefault()
-        //this.setState({change:this.state.tasks})
-        //console.log(this.state.change);
-        
-        let aa = this.state.change;
-        
+      
         if(this.state.change === '') return;
-        this.state.tasks.push(aa);
-        this.setState({tasks:[{...this.state.tasks ,  todoId: this.todoId  }]} );
-        //this.setState({tasks: this.state.tasks.push(aa)} );
-       // console.log(this.state.tasks);
+       // this.state.tasks.push(aa);
+        const shalloArr = [...this.state.tasks ,
+                    {   text: this.state.change,
+                        todoId: this.todoId
+                    }
+        ]
+        this.setState({tasks:[...shalloArr]} );
         this.setState({change:''})
-       // console.log(this.state.change);
         this.todoId++
+        
     }
     handleDelete = (id) => {
         
@@ -51,22 +45,23 @@ class ToDo extends Component {
            outline: "none"
         }
         
-        
+         console.log(this.state.tasks)
         
         return(
             <div  className="styledDiv"  onSubmit={this.handleSubmit}>
                 <h1 className='stH1' > TO DO </h1>
-            <InputGroup >
-                <Input type="text" 
-                style={styledInput}
-                placeholder='Enter Item'
-                onChange={(e) => this.setState({change: e.target.value})} />
-            <InputGroupAddon addonType="append">
-                <Button color="secondary"   onClick={this.handleSubmit} >ADD</Button>
-            </InputGroupAddon>
-            </InputGroup>
+                    <InputGroup className='inputGroup' >
+                        <Input type="text" 
+                            value={this.state.change}
+                            style={styledInput}
+                            placeholder='Enter Item'
+                            onChange={(e) => this.setState({change: e.target.value})} />
+                        <InputGroupAddon addonType="append">
+                        <Button color="secondary"   onClick={this.handleSubmit} >ADD</Button>
+                        </InputGroupAddon>
+                    </InputGroup>
         
-            <List handleDelete={this.handleDelete} tasks={ this.state.tasks} />
+                    <List handleDelete={this.handleDelete} tasks={ this.state.tasks} />
             </div>
         )
     }
